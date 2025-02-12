095d8de5fc3632ec7f6283e7839c7afb497983ac
stivi007 <steeveg371@gmail.com>
2025-02-11 23:12:17 -0400

    integracion de clases

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+
+
+
 //bucles 
 
 // let count = 0;
 // let encontrar = 2;
-// //while
+//while
 // while (count < 20) {
 //     if(encontrar < count){
 //         throw new Error("Error");
@@ -16,14 +19,14 @@
 //     count++;
 // }
 //--------------------------------------------------------------------------------------------
-//arreglos
+// arreglos
 // let arrNumbers = [1,2,3,4,5,6,7,8,9];
 // console.log('-------------------------------------------------------');
 
-// //for
-// // for (let i = 0; i < arrNumbers.length; i++) {
-// //     console.log(arrNumbers[i]);
-// // }
+//for
+// for (let i = 0; i < arrNumbers.length; i++) {
+//     console.log(arrNumbers[i]);
+// }
 // // metodo map
 // if(arrNumbers.length == 0){
 //     throw new Error('El arrelo esta basio')
@@ -33,8 +36,9 @@
 // });
 // console.log(arrNumbers2);
 
+
 //--------------------------------------------------------------------------------------------
-// //reduce
+// reduce
 // const products=[
 //     {name: 'laptop', price: 2000},
 //     {name: 'desktop', price: 1500},
@@ -53,7 +57,7 @@
 // console.log(evenNumbers);
 
 //--------------------------------------------------------------------------------------------
-//funcion que recibe un arreglo de nombres y retorna el nombre mas largo
+// funcion que recibe un arreglo de nombres y retorna el nombre mas largo
 // const largestNameOfArray=( nameArray ) =>{
 
 //     let largestName = '';
@@ -72,12 +76,12 @@
 // }
 
 
-// let heroes = ['Deadpool', 'Ciclope', 'Magneto', 'Profesor Charles Xavier']; 
+// let heroes = ['Deadpool', 'Ciclope', 'Magnetooooooooooooooooooooooooooooooooooo', 'Profesor Charles Xavier']; 
 // let hero = largestNameOfArray( heroes );
 // console.log( hero ); // Profesor Charles Xavier
 
 //--------------------------------------------------------------------------------------------
-//funcion que recibe un arreglo de nombres y retorna los nombres que empiezan con una letra en especifico
+// funcion que recibe un arreglo de nombres y retorna los nombres que empiezan con una letra en especifico
 // const heroesThatStartsWith= ( nameList, letter )=> {
    
 //     letter = letter.toUpperCase();
@@ -94,25 +98,91 @@
 
 
 // let heroes = ['Doom', 'Dr. Strange', 'Hulk', 'She Hulk', 'Spiderman', 'Captain Marvel']; 
-// let herosWithLetterS = heroesThatStartsWith( heroes, 'c' );
+// let herosWithLetterS = heroesThatStartsWith( heroes, 'd' );
 // console.log( herosWithLetterS ); // She Hulk, Spiderman
 
 //--------------------------------------------------------------------------------------------
 //destructuracion de objetos
-const person={
-    name: 'Juan',
-    age: 25,
-    isStudent: false,
-    school: 'Udemy',
-    courses: ['React', 'Node', 'MongoDB']
+// const person={
+//     names: 'Juan',
+//     age: 25,
+//     isStudent: false,
+//     school: 'Udemy',
+//     courses: ['React', 'Node', 'MongoDB']
+// }
+
+// const {name, age, isStudent, school, courses} = person;
+// console.log(`Hola soy ${names} tengo ${age} años y estudio en ${school}`);
+// console.log(`Los cursos que tome en ${school} son: ${courses}`);
+
+// // //--------------------------------------------------------------------------------------------
+// //destructuracion de arreglos
+// const numbers = [12,22,32,42,5,6,7,8,9,10];
+// const [first, second, third, fourth] = numbers;
+// console.log(first, second, third, fourth);
+
+
+// class NameAnalyzer {
+//     constructor(nameArray) {
+//         this.nameArray = nameArray;
+//     }
+
+//     largestName() {
+//         let largestName = '';
+
+//         for (let i = 0; i < this.nameArray.length; i++) {
+//             let name = this.nameArray[i];
+
+//             if (name.length > largestName.length) {
+//                 largestName = name;
+//             }
+//         }
+
+//         return largestName;
+//     }
+// }
+
+// let heroes = ['Deadpool and iron man and spider man', 'Ciclope', 'Magneto', 'Profesor Charles Xavier'];
+// let nameAnalyzer = new NameAnalyzer(heroes);
+// let hero = nameAnalyzer.largestName();
+// console.log(hero);
+
+// const heroesThatStartsWith= ( nameList, letter )=> {
+   
+//     letter = letter.toUpperCase();
+//     let newNameList = [];
+
+//     for( let name of nameList ) {
+//         if ( name.startsWith(letter) ) {
+//             newNameList.push( name );
+//         }
+//     }
+
+//     return newNameList;
+// }
+
+
+
+
+class HerosAnalyzer {
+    constructor(nameList,letter){
+        this.nameList = nameList;
+        this.letter = letter;
+    }
+    heroesThatStartsWith(){
+        this.letter = this.letter.toUpperCase();
+    let newNameList = [];
+
+    for( let name of this.nameList ) {
+        if ( name.startsWith(this.letter) ) {
+            newNameList.push( name );
+        }
+    }
+
+    return newNameList;
+    }
 }
-
-const {name, age, isStudent, school, courses} = person;
-console.log(`Hola soy ${name} tengo ${age} años y estudio en ${school}`);
-console.log(`Los cursos que tome en ${school} son: ${courses}`);
-
-//--------------------------------------------------------------------------------------------
-//destructuracion de arreglos
-const numbers = [12,22,32,42,5,6,7,8,9,10];
-const [first, second, third, fourth] = numbers;
-console.log(first, second, third, fourth);
+let heroes = ['Doom', 'Dr. Strange', 'Hulk', 'She Hulk', 'Spiderman', 'Captain Marvel']; 
+let herosAnalyzer = new HerosAnalyzer( heroes, 'h' );
+let herosWithLetterS = herosAnalyzer.heroesThatStartsWith();
+console.log( herosWithLetterS ); 
---
+++ src/functions/01-task.js
@@ -0,0 +1,44 @@
+// // funcion que recibe un arreglo de nombres y retorna el nombre mas largo
+// const largestNameOfArray=( nameArray ) =>{
+
+//     let largestName = '';
+
+//     for( let i = 0; i < nameArray.length; i ++ ) {
+
+//         let name = nameArray[i];
+
+//         if ( name.length > largestName.length ) {
+//             largestName = name;
+//         }
+
+//     }
+
+//     return largestName;
+// }
+// let heroes = ['Deadpool', 'Ciclope', 'Magnetooooooooooooooooooooooooooooooooooo', 'Profesor Charles Xavier']; 
+// let hero = largestNameOfArray( heroes );
+// console.log( hero );
+
+// export class largestNameOfArray {
+//     constructor(nameArray) {
+//         this.nameArray = nameArray;
+//     }
+
+//     getLargestName() {
+//         let largestName = '';
+
+//         for (let i = 0; i < this.nameArray.length; i++) {
+
+//             let name = this.nameArray[i];
+
+//             if (name.length > largestName.length) {
+//                 largestName = name;
+//             }
+
+//         }
+
+//         return largestName;
+//     }
+// }
+
+
